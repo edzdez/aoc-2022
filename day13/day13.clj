520027a736d1898db1f1616cafafe3ea91535467
Ethan Zhang <ezsland05@gmail.com>
2022-12-17 17:01:40 -0800

    indentation?

--- day13/day13.clj
+++ day13/day13.clj
@@ -9,15 +9,20 @@
        (filter not-empty)
        (mapv parse-packet)))
 
+; wish clojure wrapped test/expr pairs in parens like scheme, cuz this #_=> trick for indentation looks pretty horrible
+; (though it's better than the alternatives)
+; #_=> trick is from [here](https://blog.danieljanus.pl/2020/02/10/cond-indentation/)
 (defn correct-order-comp [l r]
   (cond (and (empty? l) (empty? r)) 0
         (empty? l) -1
         (empty? r) 1
         (and (int? (first l)) (int? (first r)) (= (first l) (first r))) (recur (rest l) (rest r))
-        (and (int? (first l)) (int? (first r))) (let [res (compare (first l) (first r))]
-                                                  (if (= res 0) (recur (rest l) (rest r)) res))
-        (and (vector? (first l)) (vector? (first r))) (let [res (correct-order-comp (first l) (first r))]
-                                                        (if (= res 0) (recur (rest l) (rest r)) res))
+        (and (int? (first l)) (int? (first r)))
+        #_=> (let [res (compare (first l) (first r))]
+               (if (= res 0) (recur (rest l) (rest r)) res))
+        (and (vector? (first l)) (vector? (first r)))
+        #_=> (let [res (correct-order-comp (first l) (first r))]
+               (if (= res 0) (recur (rest l) (rest r)) res))
         (int? (first l)) (recur (conj (vector (vector (first l))) (vec (rest l))) r)
         (int? (first r)) (recur l (conj (vector (vector (first r))) (vec (rest r))))))
 
