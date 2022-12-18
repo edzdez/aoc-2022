d5351271a484d88f81e9a271c2744a47dd7f477c
Ethan Zhang <ezsland05@gmail.com>
2022-12-17 16:56:08 -0800

    day13 in clj

--- day13/day13.clj
+++ day13/day13.clj
@@ -9,20 +9,20 @@
        (filter not-empty)
        (mapv parse-packet)))
 
-(defn correct-order-comp? [l r]
+(defn correct-order-comp [l r]
   (cond (and (empty? l) (empty? r)) 0
         (empty? l) -1
         (empty? r) 1
         (and (int? (first l)) (int? (first r)) (= (first l) (first r))) (recur (rest l) (rest r))
         (and (int? (first l)) (int? (first r))) (let [res (compare (first l) (first r))]
                                                   (if (= res 0) (recur (rest l) (rest r)) res))
-        (and (vector? (first l)) (vector? (first r))) (let [res (correct-order-comp? (first l) (first r))]
+        (and (vector? (first l)) (vector? (first r))) (let [res (correct-order-comp (first l) (first r))]
                                                         (if (= res 0) (recur (rest l) (rest r)) res))
         (int? (first l)) (recur (conj (vector (vector (first l))) (vec (rest l))) r)
         (int? (first r)) (recur l (conj (vector (vector (first r))) (vec (rest r))))))
 
 (defn correct-order? [window]
-  (correct-order-comp? (first window) (second window)))
+  (correct-order-comp (first window) (second window)))
 
 (defn part-1 [data]
   (->> data
@@ -33,7 +33,7 @@
 
 (defn part-2 [data]
   (let [ordered (->> (conj data [[2]] [[6]])
-                     (sort correct-order-comp?)
+                     (sort correct-order-comp)
                      vec)
         i (+ (.indexOf ordered [[2]]) 1)
         j (+ (.indexOf ordered [[6]]) 1)]
